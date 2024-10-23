af48f123ff16e05058d522e155c2452cab22edbc
pastushkov <dimaspas62@gmail.com>
2024-10-23 22:19:12 +0300

    init

--- settings.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    uiPort: process.env.PORT || 1880,
-    flowFile: 'flows.json',
-    credentialSecret: "my-secret",
-    httpAdminRoot: "/admin",
-    httpNodeRoot: "/api",
-}
