911aa5cf3f0c36d4d6a79e086241350625356166
pastushkov <dimaspas62@gmail.com>
2024-10-23 22:28:52 +0300

    add setting example

---
+++ settings.example.js
@@ -0,0 +1,12 @@
+module.exports = {
+    functionGlobalContext: {
+      TELEGRAM_BOT_TOKEN: "",
+      DB_HOST: "",
+      DB_PORT: "",
+      DB_NAME: "",
+      DB_USER: "",
+      DB_PASSWORD: "",
+    },
+    flowFile: "flows.json",
+  };
+  
